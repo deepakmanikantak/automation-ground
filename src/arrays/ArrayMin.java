402dd550e56d898c63f767e47c80ef477a76e5c9
deepak <deepakmanikanta.k@gmail.com>
2021-02-21 17:10:03 +0530

    Arrays

---
+++ src/arrays/ArrayMin.java
@@ -0,0 +1,55 @@
+package arrays;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ArrayMin {
+
+    public static void main(String[] args) throws Exception {
+
+
+
+        // Database db = new Database();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the no elements you want to enter: ");
+        try {
+
+            // Query
+
+            int size = scanner.nextInt();
+            int[] inputRollNumber = new int[size];
+            // Array should be filled
+//            for (int i =0 ;  i< inputRollNumber.length; i++){
+//                System.out.println("Enter the the Element "+(i+1)+": ");
+//                inputRollNumber[i] = scanner.nextInt();
+//            }
+
+            int a =10;
+            int b = 5;
+
+            // NumberFormatException
+            // UnDevisisableException
+        } catch (Exception e) {
+        } finally {
+        //    db.connectionClose();
+            System.out.println("I am Final block.");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public void methodName() {
+
+    }
+}
